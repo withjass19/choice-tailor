62b686a3c6b73aae24295beae37e215fc0ea1f23
Jaspreet Singh <95354014+withjass19@users.noreply.github.com>
2026-06-27 01:08:06 +0530

    choice tailor

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ import Categories from "./pages/Admin/Categories/Categories";
 import Inventory from "./pages/Admin/Inventory/Inventory";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminRoute from "./routes/AdminRoute";
-import Contact from "./pages/Contact/Contact";
+// import Contact from "./pages/Contact/Contact";
 
 export default function App() {
   return (
@@ -38,7 +38,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact/>} />
+        {/* <Route path="/contact" element={<Contact/>} /> */}
         
         {/* <Route path="/contact" element={<Contact/>} /> */}
         {/* <Route path="/track-order" element={<TrackOrder />} /> */}
