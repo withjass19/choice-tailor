db6a36ba4e30f6e77f881a2e551d3d2450eefc9e
Jaspreet Singh <95354014+withjass19@users.noreply.github.com>
2026-06-27 09:40:31 +0530

    update imagekit-auth3

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -2,23 +2,10 @@ import ImageKit from "imagekit";
 
 export default function handler(req, res) {
   try {
-    if (
-      !import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY ||
-      !import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY ||
-      !import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT
-    ) {
-      return res.status(400).json({
-        error: "Missing ImageKit env variables",
-        hasPublicKey: !!import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
-        hasPrivateKey: !!import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY,
-        hasUrlEndpoint: !!import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
-      });
-    }
-
     const imagekit = new ImageKit({
-      publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
-      privateKey: import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY,
-      urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
     });
 
     const authParams = imagekit.getAuthenticationParameters();
