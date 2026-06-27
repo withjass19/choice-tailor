3c9f6ab684b87d1db154c1486718f8d572b7df86
Jaspreet Singh <95354014+withjass19@users.noreply.github.com>
2026-06-27 09:37:36 +0530

    update imagekit-auth2

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -3,22 +3,22 @@ import ImageKit from "imagekit";
 export default function handler(req, res) {
   try {
     if (
-      !import.meta.env.IMAGEKIT_PUBLIC_KEY ||
-      !import.meta.env.IMAGEKIT_PRIVATE_KEY ||
-      !import.meta.env.IMAGEKIT_URL_ENDPOINT
+      !import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY ||
+      !import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY ||
+      !import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT
     ) {
       return res.status(400).json({
         error: "Missing ImageKit env variables",
-        hasPublicKey: !!import.meta.env.IMAGEKIT_PUBLIC_KEY,
-        hasPrivateKey: !!import.meta.env.IMAGEKIT_PRIVATE_KEY,
-        hasUrlEndpoint: !!import.meta.env.IMAGEKIT_URL_ENDPOINT,
+        hasPublicKey: !!import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
+        hasPrivateKey: !!import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY,
+        hasUrlEndpoint: !!import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
       });
     }
 
     const imagekit = new ImageKit({
-      publicKey: import.meta.env.IMAGEKIT_PUBLIC_KEY,
-      privateKey: import.meta.env.IMAGEKIT_PRIVATE_KEY,
-      urlEndpoint: import.meta.env.IMAGEKIT_URL_ENDPOINT,
+      publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
+      privateKey: import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
     });
 
     const authParams = imagekit.getAuthenticationParameters();
