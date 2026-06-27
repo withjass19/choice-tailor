ad280dc9f6be724584806891eaaf96ca3defeea6
Jaspreet Singh <95354014+withjass19@users.noreply.github.com>
2026-06-27 09:07:54 +0530

    add  navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import TopBar from "@/components/TopBar/TopBar";
+import NavBar from "@/components/NavBar/NavBar";
 
 
 export default function MainLayout() {
   return (
     <>
       <TopBar />
-
+      <NavBar/>
       <main>
         <Outlet />
       </main>
